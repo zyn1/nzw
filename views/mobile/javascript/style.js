3788e8f22e56475a2c0135e8572fcdb31b3e7e79
ducong123 <dcwode123@163.com>
2016-12-01 11:41:04 +0800

    购物车全选js

--- views/mobile/javascript/style.js
+++ views/mobile/javascript/style.js
@@ -26,15 +26,57 @@
              $(this).parent().parent().parent().parent(".cart_sel").find(".cart_edit").show();
              $(this).parent().parent().parent().parent(".cart_sel").find(".cart_goods").hide();
       })
-         /*购物车选择*/
+         /*购物车选择 start*/
       $(".checkbox").click(function(){
-       if($(this).find("i").hasClass("check")){
-        $(this).find("i").removeClass("check");
-       }else{
-        $(this).find("i").addClass("check");
-        } 
-        
+        $(this).find("i").toggleClass("check");
+        var all_boxi=$(this).parent().parent("ul").find("li .checkbox i");
+        var sel_all_i=$(this).parent().parent().parent(".cart_goods").find(".cart_title .sel_all i");
+        var all_checki=$(this).parent().parent("ul").find("li .checkbox .check");
+        //.cart_list 下的class="box"下的i标签存在class="check"的值
+        var box_all_checki= $(".cart_goods").find(".checkbox .check");
+        //.cart_list 下的class="box"下的i标签
+        var box_all_onchecki=$(".cart_goods").find(".checkbox i")
+        //全选按钮
+        var box_all_i = $(".box_all i");
+        if(all_boxi.length == all_checki.length){
+          sel_all_i.addClass('check');
+        }else{
+          sel_all_i.removeClass("check");
+        }
+        if(box_all_onchecki.length == box_all_checki.length ){
+          box_all_i.addClass('check');
+        }else{
+          box_all_i.removeClass("check");
+        }
       });
+      $(".sel_all").click(function(){
+          $(this).find("i").toggleClass("check");
+          var checkbox_i=$(this).parent().parent(".cart_goods").find("ul li .checkbox i")
+          var sel_check_i=$(".cart_goods .cart_title").find(".sel_all .check");
+          var sel_oncheck_i=$(".cart_goods .cart_title").find(".sel_all i");
+          //全选按钮
+          var box_all_i2 = $(".box_all i");
+          if($(this).find("i").hasClass("check")){
+            checkbox_i.addClass("check");
+          }else{
+            checkbox_i.removeClass("check");
+          }
+          if(sel_check_i.length == sel_oncheck_i.length){
+            box_all_i2.addClass('check');
+          }else{
+            box_all_i2.removeClass("check");
+          }
+      })
+      $(".box_all").click(function(){
+        $(this).find("i").toggleClass("check");
+        var all_box_checki=$(".cart_list").find(".box i")
+        if($(this).find("i").hasClass("check")){
+          all_box_checki.addClass("check");
+        }else{
+            all_box_checki.removeClass("check");
+        }
+      })
+      /*购物车选择 end*/
       //发票类型选择
       $(".invoic_top .top_title .sj_de").click(function(){
         $(".invoic_zp").hide();
