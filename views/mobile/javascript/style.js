07cab4cde48a9b4971cd14cdf98384cbb6e567aa
ducong123 <dcwode123@163.com>
2017-01-12 12:04:22 +0800

    修改appbug

--- views/mobile/javascript/style.js
+++ views/mobile/javascript/style.js
@@ -17,5 +17,18 @@
         $(".invoic_top .sj_de").removeClass("on");
         $('div.invoic_center').find('input[type=hidden][name=fapiao_type]').val(1);
       });
-
+//修改placeholder字体颜色
+ $('[placeholder]').focus(function() { 
+    var input = $(this); 
+    if (input.val() == input.attr('placeholder')) { 
+    input.val(''); 
+    input.removeClass('placeholder'); 
+    } 
+    }).blur(function() { 
+    var input = $(this); 
+    if (input.val() == '' || input.val() == input.attr('placeholder')) { 
+    input.addClass('placeholder'); 
+    input.val(input.attr('placeholder')); 
+    } 
+    }).blur(); 
 });
